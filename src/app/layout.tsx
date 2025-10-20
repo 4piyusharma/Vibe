bd962a10244d39e3fe534d530e77768218a96d85
04piyush <4piyusharma@gmail.com>
2025-10-20 17:56:25 +0530

    01: setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" katalonextensionid="ljdobmomdgdljniojadhoplhkpialdid">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
